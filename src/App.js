f6153814f4b601ff3496dd33f100597f73199183
KhoiNgLe <nguy0572@algonquinlive.com>
2022-01-12 22:41:40 -0500

    using map method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   const[name, setName] = useState('Mario')
 
+  const[events, setEvents] = useState([
+    {title: "hello world", id: 1},
+    {title: "day 1", id: 2},
+    {title: "day 2", id: 3}
+  ])
+
 
   const handleClick = () =>{
     setName("Louis")
@@ -14,6 +20,12 @@ function App() {
     <div className="App">
         <h1>My name is {name}</h1>
         <button onClick = {handleClick}>Change name</button>
+        {events.map((event, index)=> (
+          <div key = {event.id}>
+            <h2>{index} - {event.title}</h2>
+          </div>
+        ))}
+
     </div>
   );
 }
