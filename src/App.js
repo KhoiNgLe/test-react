189c486ca3eb656de9c1f2ae6b7faadf8c747823
KhoiNgLe <nguy0572@algonquinlive.com>
2022-01-16 22:22:14 -0500

    Add props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import { useState } from 'react'
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Title from "./Title.js"
 
 function App() {
-  const[name, setName] = useState('Mario')
+  const [showEvents, setShowEvents] = useState(true);
+  const [name, setName] = useState("Mario");
 
-  const[events, setEvents] = useState([
-    {title: "hello world", id: 1},
-    {title: "day 1", id: 2},
-    {title: "day 2", id: 3}
-  ])
+  const [events, setEvents] = useState([
+    { title: "hello world", id: 1 },
+    { title: "day 1", id: 2 },
+    { title: "day 2", id: 3 },
+  ]);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
+  };
+
+  console.log(showEvents);
 
 
-  const handleClick = () =>{
-    setName("Louis")
-    console.log(name)
-  }
+const subtitle = "All the lastes events in Marioland"
 
   return (
     <div className="App">
-        <h1>My name is {name}</h1>
-        <button onClick = {handleClick}>Change name</button>
-        {events.map((event, index)=> (
-          <div key = {event.id}>
-            <h2>{index} - {event.title}</h2>
+      <Title title = "Events in Your Area" subtitle = {subtitle}/>
+      <Title title = "another title" subtitle = "Another subtitle"/>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>Delete Event</button>
           </div>
         ))}
-
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,22 @@
+
+
+
+const Title = (props) =>{
+    return(
+        <div>
+            <h1 className="title" >{props.title}</h1>
+            <br/>
+            <h2 className ="subtitle">{props.subtitle} </h2>
+        </div>
+    )
+
+}
+
+
+
+
+
+
+
+
+export default Title
